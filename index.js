33cfd78d273e892f59f174dd0b721c75c9d74936
Ariel Henryson <ariel@codefresh.io>
2021-09-12 11:53:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,128 @@
+const fs = require('fs')
+const colors = require('colors')
+
+const originalData = process.env.PR_DATA || fs.readFileSync('./test.md', 'utf-8')
+const lines = originalData.split('\n')
+
+const results = {
+    type: {
+        bugFix: getValue('bugFix'),
+        newFeature: getValue('newFeature'),
+        breakingChange: getValue('breakingChange'),
+    },
+    affectedAreas: {
+        analyticsReporter: getValue('analyticsReporterChange'),
+        apiEvents: getValue('apiEventsChange'),
+        apiGraphql: getValue('apiGraphqlChange'),
+        e2e: getValue('e2eChanges'),
+        eventHandler: getValue('eventHandlerChange'),
+        graphQLSchema: getValue('graphQLSchemaChange'),
+        database: getValue('databaseLayerChange'),
+        eventBus: getValue('eventBusChange'),
+        tooling: getValue('toolingChange'),
+    },
+    specialInformation: {
+        newLib: getValue('newLib'),
+        newIntegration: getValue('newIntegration'),
+        newEnvVar: getValue('newEnvVar'),
+    },
+    checklist: [
+        getValue('checklist1'),
+        getValue('checklist2'),
+        getValue('checklist3'),
+        getValue('checklist4'),
+        getValue('checklist5'),
+        getValue('checklist6'),
+        getValue('checklist7'),
+    ]
+}
+
+
+function getValue(id) {
+    for (const line of lines) {
+        const selectedLine = line.includes('#' + id)
+        if (selectedLine) {
+            return !!(line.includes('[X]') || line.includes('[x]'))
+        }
+    }
+}
+
+function validatedType() {
+    let noType = true
+
+    if (
+        results.type.bugFix ||
+        results.type.newFeature ||
+        results.type.breakingChange
+    ) {
+        noType = false
+    }
+
+    if (noType) {
+        console.error('Please specify the type of this PR'.red)
+        process.exit(1)
+    }
+}
+
+function validatedAreas() {
+    let noArea = true
+
+    if (
+        results.affectedAreas.analyticsReporter ||
+        results.affectedAreas.apiEvents ||
+        results.affectedAreas.apiGraphql ||
+        results.affectedAreas.e2e ||
+        results.affectedAreas.eventHandler ||
+        results.affectedAreas.graphQLSchema ||
+        results.affectedAreas.database ||
+        results.affectedAreas.eventBus ||
+        results.affectedAreas.tooling
+    ) {
+        noArea = false
+    }
+
+    if (noArea) {
+        console.error('Please specify The affected area for this PR'.red)
+        process.exit(1)
+    }
+}
+
+function validatedSpecialInformation() {
+    if (results.specialInformation.newLib) {
+        console.error('This PR requires special attention'.bgMagenta)
+        console.error('There is a new library in this PR'.bgMagenta)
+        console.log('\n')
+    }
+
+    if (results.specialInformation.newIntegration) {
+        console.error('This PR requires special attention'.bgMagenta)
+        console.error('There is a new integration in this PR'.bgMagenta)
+        console.log('\n')
+    }
+
+    if (results.specialInformation.newEnvVar) {
+        console.error('This PR requires special attention'.bgMagenta)
+        console.error('This PR required to set up environment variable'.bgMagenta)
+        console.log('\n')
+    }
+}
+
+function validatedCheckList() {
+    let checkPass = true
+
+    for (const item of results.checklist) {
+        if (!item) checkPass = false
+    }
+
+    if (!checkPass) {
+        console.error(`You didn't complete the Pull request checklist`.red)
+        process.exit(1)
+    }
+}
+
+validatedType()
+validatedAreas()
+validatedCheckList()
+validatedSpecialInformation()
+
+console.log('PR checklist pass successfully'.green)
---
+++ test.md
@@ -0,0 +1,32 @@
+# Pull Request template
+
+## Type of change
+- [ ] Bug fix (non-breaking change which fixes an issue) <!-- #bugFix -->
+- [ ] New feature (non-breaking change which adds functionality) <!-- #newFeature -->
+- [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected) <!-- #breakingChange -->
+- [ ] This change requires a documentation update <!-- #documentationChangeRequires -->
+
+## Affected areas
+- [ ] analytics-reporter <!-- #analyticsReporterChange -->
+- [ ] api-events <!-- #apiEventsChange -->
+- [ ] api-graphql <!-- #apiGraphqlChange -->
+- [ ] e2e <!-- #e2eChanges -->
+- [ ] event-handler <!-- #eventHandlerChange -->
+- [ ] GraphQL schema <!-- #graphQLSchemaChange -->
+- [ ] Database layer <!-- #databaseLayerChange -->
+- [ ] EventBus <!-- #eventBusChange -->
+- [ ] Tooling (lint, Lerna etc...) <!-- #toolingChange -->
+
+# Special information
+- [ ] There is a new library in this PR <!-- #newLib -->
+- [ ] There is a new integration in this PR <!-- #newIntegration -->
+- [ ] This PR Require a new environment variable <!-- #newEnvVar -->
+
+## Checklist:
+- [ ] My code follows the style guidelines of this project <!-- #checklist1 -->
+- [ ] I have performed a self-review of my own code <!-- #checklist2 -->
+- [ ] My changes generate no new warnings <!-- #checklist3 -->
+- [ ] I have added tests that prove my fix is effective or that my feature works <!-- #checklist4 -->
+- [ ] New and existing unit tests pass locally with my changes <!-- #checklist5 -->
+- [ ] Any dependent changes have been merged and published in downstream modules <!-- #checklist6 -->
+- [ ] I have checked my code and corrected any misspellings <!-- #checklist7 -->
